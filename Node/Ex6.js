7d38b629f978ab46a841a2827b25de9838cc856e
ilay <ilay@Ilays-MacBook-Pro-2.local>
2018-01-17 21:46:27 +0200

    arrange folders

--- Node/Ex6.js
+++
@@ -1,8 +0,0 @@
-function rotate(word) {
-	let length = word.length;
-	for (let i = 0; i < length; i++) {
-		console.log(word);
-		word = word.substring(length - 1, length) + word.substring(0 , length - 1);
-	}
-}
-rotate("milab");
